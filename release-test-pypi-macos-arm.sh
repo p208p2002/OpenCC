32fadbf8a09255701c3684899325db68cf094f7d
p208p2002 <p208p2002@gmail.com>
2022-09-21 16:40:16 +0800

    Remove py3.7

--- release-test-pypi-macos-arm.sh
+++ release-test-pypi-macos-arm.sh
@@ -12,7 +12,7 @@ bash ${MINICONDA_FILENAME} -b -f -p $HOME/miniconda3
 export PATH=$HOME/miniconda3/bin:$PATH
 eval "$(conda shell.bash hook)"
 
-for VERSION in 3.7 3.8 3.9 3.10; do
+for VERSION in 3.8 3.9 3.10; do
     # Create and activate environment
     CONDA_SUBDIR=osx-arm64 conda create -y -n py$VERSION python=$VERSION
     conda activate py$VERSION
